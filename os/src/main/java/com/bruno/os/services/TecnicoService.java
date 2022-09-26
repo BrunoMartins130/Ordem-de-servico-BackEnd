8b2c12ad0b1eb5ca6cdc26f1d56e556dd7e9f3eb
Bruno Martins <brunomartinsdasilva96@hotmail.com>
2022-09-26 20:27:26 -0300

    lancamento de excecao de cpf ja cadastrado para o banco de dados

--- os/src/main/java/com/bruno/os/services/TecnicoService.java
+++ os/src/main/java/com/bruno/os/services/TecnicoService.java
@@ -1,7 +1,9 @@
 package com.bruno.os.services;
 
 import com.bruno.os.domain.Tecnico;
+import com.bruno.os.dtos.TecnicoDTO;
 import com.bruno.os.repositories.TecnicoRepository;
+import com.bruno.os.services.exceptions.DataIntegratyViolationException;
 import com.bruno.os.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,4 +25,20 @@ public class TecnicoService {
     public List<Tecnico> findAll() {
         return repository.findAll();
     }
+    public Tecnico create(TecnicoDTO objDTO) {
+        if(findByCPF(objDTO) != null) {
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+        }
+
+        return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(),objDTO.getTelefone()));
+    }
+
+    private Tecnico findByCPF(TecnicoDTO objDTO) {
+        Tecnico obj = repository.findByCPF(objDTO.getCpf());
+        if(obj != null) {
+            return obj;
+        }
+        return null;
+    }
+
 }
