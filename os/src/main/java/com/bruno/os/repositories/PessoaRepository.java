aa0c42d3b17541ad78aa3650e113c2ccd7eb3313
Bruno Martins <brunomartinsdasilva96@hotmail.com>
2022-09-29 20:28:54 -0300

    ajuste no metodo findByCpf para filtro dos dados de Clientes tambem

--- os/src/main/java/com/bruno/os/repositories/PessoaRepository.java
+++ os/src/main/java/com/bruno/os/repositories/PessoaRepository.java
@@ -1,13 +1,13 @@
 package com.bruno.os.repositories;
 
 import com.bruno.os.domain.Pessoa;
-import com.bruno.os.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TecnicoRepository extends JpaRepository<Tecnico , Integer> {
-
+public interface PessoaRepository extends JpaRepository<Pessoa , Integer> {
+    @Query("SELECT obj FROM Pessoa obj WHERE obj.cpf =:cpf")
+    Pessoa findByCPF(@Param("cpf") String cpf);
 }
--- os/src/main/java/com/bruno/os/services/TecnicoService.java
+++ os/src/main/java/com/bruno/os/services/TecnicoService.java
@@ -1,7 +1,9 @@
 package com.bruno.os.services;
 
+import com.bruno.os.domain.Pessoa;
 import com.bruno.os.domain.Tecnico;
 import com.bruno.os.dtos.TecnicoDTO;
+import com.bruno.os.repositories.PessoaRepository;
 import com.bruno.os.repositories.TecnicoRepository;
 import com.bruno.os.services.exceptions.DataIntegratyViolationException;
 import com.bruno.os.services.exceptions.ObjectNotFoundException;
@@ -17,14 +19,26 @@ public class TecnicoService {
     @Autowired
     private TecnicoRepository repository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+    /*
+     * Busca Tecnico pelo id
+     */
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!" + id + ", Tipo: " + Tecnico.class.getName()));
     }
 
+    /*
+     * Busca todos os Tecnicos da Base de dados
+     */
     public List<Tecnico> findAll() {
         return repository.findAll();
     }
+
+    /*
+     * Cria um Tecnico
+     */
     public Tecnico create(TecnicoDTO objDTO) {
         if(findByCPF(objDTO) != null) {
             throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
@@ -32,6 +46,10 @@ public class TecnicoService {
 
         return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(),objDTO.getTelefone()));
     }
+    /*
+     * Atualiza um tecnico pelo ID
+     */
+
     public Tecnico update(Integer id, TecnicoDTO objDTO) {
         Tecnico oldObj = findById(id);
 
@@ -44,6 +62,9 @@ public class TecnicoService {
         return repository.save(oldObj);
     }
 
+    /*
+     * Deleta um Tecnico pelo ID
+     */
     public void delete(Integer id) {
         Tecnico obj = findById(id);
         if(obj.getList().size() > 0) {
@@ -52,8 +73,12 @@ public class TecnicoService {
         repository.deleteById(id);
 
     }
-    private Tecnico findByCPF(TecnicoDTO objDTO) {
-        Tecnico obj = repository.findByCPF(objDTO.getCpf());
+
+    /*
+     * Busca Tecnico pelo CPF
+     */
+    private Pessoa findByCPF(TecnicoDTO objDTO) {
+        Pessoa obj = pessoaRepository.findByCPF(objDTO.getCpf());
         if(obj != null) {
             return obj;
         }
